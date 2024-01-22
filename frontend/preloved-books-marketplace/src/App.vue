cade25099ef1b45ef90fad683aa0154022f6dc70
yuning-dev <yuning.eng@gmail.com>
2024-01-22 11:04:58 +0000

    create initial frontend folder structure

---
+++ frontend/preloved-books-marketplace/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/"></RouterLink>
+        <RouterLink to="/about"></RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
